fdadc0e8272efbfeac1104200b9d126128a809a7
ASekretarski <artur.sekretarski@gmail.com>
2023-05-15 16:36:27 +0200

    task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listOfIngredients = document.querySelector('#ingredients')
+ingredients.forEach((item) => {
+  const ingredient = document.createElement("li")
+  ingredient.textContent = item
+  ingredient.classList.add("item")
+  listOfIngredients.append(ingredient)
+})
