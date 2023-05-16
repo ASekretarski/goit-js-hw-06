0171bb0a9c6f44eb8da428ca3becaaaf5ed39eaf
ASekretarski <artur.sekretarski@gmail.com>
2023-05-16 20:35:23 +0200

    task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const createButton = document.querySelector('[data-create]')
+const destroyButton = document.querySelector('[data-destroy]')
+const amountOfBoxes = document.querySelector('input')
+const placeForBoxes = document.querySelector('#boxes')
+
+function createBoxes(amount) {
+  amount=amountOfBoxes.value
+  let size = 20;
+  for (let i = 0; i < amount; i++){
+    size += 10;
+    placeForBoxes.insertAdjacentHTML("beforeend", `<div style="height: ${size}px; width: ${size}px;background-color: ${getRandomHexColor()}"></div>`)
+  }
+}
+function destroyBoxes() {
+  placeForBoxes.innerHTML=''
+}
+
+createButton.addEventListener("click", createBoxes)
+destroyButton.addEventListener("click", destroyBoxes)
