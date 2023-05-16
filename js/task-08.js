c17e23cdeca8a699351303a2f47f03279e68d836
ASekretarski <artur.sekretarski@gmail.com>
2023-05-16 19:32:50 +0200

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form')
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value==="") {
+        alert('All fields must be filled, please.');
+        return
+    }
+    const formData = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(formData)
+    event.currentTarget.reset()
+}
+
+form.addEventListener("submit", handleSubmit)
